08e27ae0de72916c705e9b58e1588c475d32765d
Brian Douglas <ilikerobot@gmail.com>
2016-01-25 10:09:36 -0800

    partial messages

--- App/Components/MessageThreads.js
+++ App/Components/MessageThreads.js
@@ -1,5 +1,5 @@
 import React from 'react-native';
-// import Separator from './../Helpers/Separator';
+import Separator from './../Helpers/Separator';
 import api from './../Lib/Api';
 
 let {
@@ -11,9 +11,19 @@ let {
 
 let styles = StyleSheet.create({
   container: {
-    height: 500,
-    backgroundColor: 'red',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'stretch',
+    height: 590,
     marginTop: 65,
+  },
+  rowContainer: {
+    padding: 10,
+  },
+  message: {
+    flex: 2,
+    fontSize: 22,
+    padding: 15,
   },
 });
 
@@ -24,32 +34,54 @@ export default class MessageThreads extends React.Component{
     this.ds = new ListView.DataSource({rowHasChanged: (row1, row2) => row1 !== row2})
     this.state = {
       isLoading: true,
+      data: '',
       empty: false,
       error: '',
     }
   }
 
   componentDidMount() {
-    this.fetchMessages();
+    this.fetchFakeData();
+  }
+
+  fetchFakeData() {
+    let data = api.fakeIt();
+
+    this.setState({
+      data: data.items,
+      dataSource: this.ds.cloneWithRows(data.items),
+      isLoading: false,
+      empty: false,
+     });
   }
 
   fetchMessages() {
-    console.log('hit it')
+    // TODO: add when fake data works
     api.getMessages()
-    .then(() => {
-      console.log('YOOOO');
-      // this.setState({
-      //    dataSource: this.ds.cloneWithRows(data),
-      //    isLoading: false,
-      //    empty: false,
-      //  });
-    })
     .catch((error) => console.log('NOOOO'));
+  }
+
+  renderRow(rowData) {
+    return (
+      <View>
+        <View style={styles.rowContainer}>
+          <Text style={styles.message}> {rowData.subject} </Text>
+        </View>
+        <Separator />
+      </View>
+    );
+  }
+
+  stopHere() {
+    debugger;
   }
 
   render() {
     return (
       <View style={styles.container}>
+        <ListView
+          dataSource={this.state.dataSource}
+          renderRow={this.renderRow.bind(this)} />
       </View>
     );
   }
--- App/Lib/Api.js
+++ App/Lib/Api.js
@@ -23,6 +23,36 @@ let api = {
     .catch((error) => console.log('token creation failure'));
   },
 
+  fakeIt() {
+    return {
+      items: [
+        {
+          id: 1,
+          subject: 'Hey there',
+          messages_count: 10,
+          updated_at: '2015-09-26T19:30:49.772-04:00',
+          user_image: 'https://cdn.bloc.io/path/to/image',
+          preview: 'Hey there, I was confused about this...',
+          first_name: 'Mike',
+          last_name: 'Jewett',
+          unread: true
+        },
+        {
+          id: 2,
+          subject: 'Yo',
+          messages_count: 10,
+          updated_at: '2015-09-26T19:31:49.772-04:00',
+          user_image: 'https://cdn.bloc.io/path/to/image',
+          preview: 'Hey there, I was confused about this...',
+          first_name: 'Bill',
+          last_name: 'Paxton',
+          unread: true
+        },
+      ],
+      count: 2
+    }
+  },
+
   getMessages() {
     store.get('session').then((session) => {
       let auth_token = session.token;
