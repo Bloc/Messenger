bf1ff5f54cdc18c9ebe0e08da5f47b75bbc55773
Brian Douglas <ilikerobot@gmail.com>
2016-02-16 06:35:17 -0800

    fixes api request for message threads

--- App/Lib/Api.js
+++ App/Lib/Api.js
@@ -23,43 +23,18 @@ let api = {
     .catch((error) => console.log('token creation failure'));
   },
 
-  fakeIt() {
-    return Promise.all(
-      [
-        {
-          id: 1,
-          subject: 'Hey there',
-          messages_count: 10,
-          updated_at: '2015-09-26T19:30:49.772-04:00',
-          user_image: 'https://cdn.bloc.io/path/to/image',
-          preview: 'Hey there, I was confused about this...',
-          first_name: 'Mike',
-          last_name: 'Jewett',
-          unread: true
-        },
-        {
-          id: 2,
-          subject: 'Yo',
-          messages_count: 10,
-          updated_at: '2015-09-26T19:31:49.772-04:00',
-          user_image: 'https://cdn.bloc.io/path/to/image',
-          preview: 'Hey there, I was confused about this...',
-          first_name: 'Bill',
-          last_name: 'Paxton',
-          unread: true
-        },
-      ]
-    );
-  },
-
   getMessages() {
     store.get('session').then((session) => {
       let auth_token = session.token;
-      let url = `https://www.bloc.io/api/v1/message_threads/?page=1"`;
-      fetch(url, {
-        method: 'POST',
-        body: auth_token,
-      })
+      let url = `https://www.bloc.io/api/v1/message_threads`;
+      // let url = `http://localhost:3000/api/v1/message_threads.json`;
+      let init = {
+        method: 'GET',
+        headers: {
+          'Authorization': `Bearer ${auth_token}`
+        }
+      };
+      fetch(url, init)
       .then((res) => {
         console.info(`messages retrieved`)
         console.log(res)
