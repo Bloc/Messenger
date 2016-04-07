a1120ed936bf3f281c3cdadcc4a24adb912c0a55
Brian Douglas <ilikerobot@gmail.com>
2016-04-07 07:35:00 -0700

    passing Login tests

--- App/Components/__tests__/Login.test.js
+++ App/Components/__tests__/Login.test.js
@@ -1,17 +1,33 @@
-import React, {View, TextInput} from 'react-native';
+import React, {Text, TextInput, TouchableHighlight} from 'react-native';
 import {shallow} from 'enzyme';
 import Login from '../Login';
 import {expect} from 'chai';
 
 describe('<Login />', () => {
-  it('should render a line', () => {
+  it('should render text for Login', () => {
     const component = shallow(<Login />);
-    expect(component.find(View)).to.have.length(1);
+    const text = component.find(Text).node.props.children;
+    expect(text).to.eq(' Login using your Bloc.io username and password');
   });
 
   it('populates email field', () => {
     const component = shallow(<Login />);
     component.setState({username: 'bill@mail.com'});
     expect(component.contains(<TextInput value='bill@email.com' />));
+  });
+
+  it('populates password field', () => {
+    const component = shallow(<Login />);
+    component.setState({password: 'top_secret'});
+    expect(component.contains(<TextInput value='top_secret' />));
+  });
+
+  xit('form renders 2nd text for error when submited without info', () => {
+    const component = shallow(<Login />);
+    button = component.find(TouchableHighlight);
+    button.simulate('click');
+
+    const text = component.find(Text);
+    expect(text.length).to.equal(2);
   });
 });
